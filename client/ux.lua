e62209774ad7babbd791c2e2eb1558d1e7c55a10
reimu <r2hkri@gmail.com>
2024-03-30 14:10:26 -0500

    initial commit

---
+++ client/ux.lua
@@ -0,0 +1,121 @@
+local currentJobs = {}
+local currentJobProgress = 0
+local honkProgress = 0
+local deliveryPoint = nil
+local timer = nil
+
+addEvent(g_SHOW_JOB, true)
+addEventHandler(g_SHOW_JOB, resourceRoot, function(id, type, pos)
+	local job = g_JOBS_BY_TYPE[type]
+
+	currentJobs[id] = {
+		marker = createMarker(pos.x, pos.y, pos.z - 0.6, "cylinder", job.zoneRadius, job.color.r, job.color.g, job.color.b, 73),
+		blip = createBlip(pos.x, pos.y, pos.z, job.blip, 2, 255, 0, 0, 255, 0, job.detectionRadius),
+	}
+end)
+
+addEvent(g_HIDE_JOB, true)
+addEventHandler(g_HIDE_JOB, resourceRoot, function(id)
+	-- could modify alpha instead if we separate things differently
+	if not currentJobs[id] then return end
+
+	destroyElement(currentJobs[id].marker)
+	destroyElement(currentJobs[id].blip)
+
+	currentJobs[id] = nil
+end)
+
+addEvent(g_START_JOB, true)
+addEventHandler(g_START_JOB, resourceRoot, function(id)
+end)
+
+addEvent(g_STOP_JOB, true)
+addEventHandler(g_STOP_JOB, resourceRoot, function(id)
+	currentJobProgress = 0
+	honkProgress = 0
+	if isTimer(timer) then
+		killTimer(timer)
+	end
+end)
+
+addEvent(g_JOB_STATUS_UPDATE, true)
+addEventHandler(g_JOB_STATUS_UPDATE, resourceRoot, function(id, type, data)
+	if type == g_DELIVERY_JOB.type then
+		-- delivery job target
+		local pickupSize = 10
+		local col = createColCircle(data.pos.x, data.pos.y, pickupSize)
+
+		local marker = createMarker(data.pos.x, data.pos.y, data.pos.z - 0.6, "cylinder", pickupSize, 255, 0, 0, 100)
+		local blip = createBlip(data.pos.x, data.pos.y, data.pos.z, 29, 2, 255, 0, 0, 255)
+
+		function finishDelivery(element)
+			if getPedOccupiedVehicle(localPlayer) ~= element then return end
+			destroyElement(marker)
+			destroyElement(blip)
+			removeEventHandler("onClientColShapeHit", col, finishDelivery)
+			triggerServerEvent(g_FINISH_JOB, resourceRoot, id)
+		end
+		addEventHandler("onClientColShapeHit", col, finishDelivery)
+	elseif type == g_EXTORTION_JOB.type then
+		outputChatBox("START HONKING!!!")
+		function honk()
+			honkProgress = math.min(honkProgress + 0.1, 1)
+		end
+		bindKey("horn", "down", honk)
+		timer = setTimer(function()
+			if honkProgress >= 1 then
+				unbindKey("horn", "down", honk)
+				if isTimer(timer) then
+					killTimer(timer)
+					triggerServerEvent(g_FINISH_JOB, resourceRoot, id)
+				end
+			end
+
+			-- 5 honks a second at least?
+			honkProgress = math.max(honkProgress - 0.01, 0)
+		end, 200, 0)
+	elseif data.progress then
+		-- group or solo camp jobs
+		currentJobProgress = data.progress
+	end
+end)
+
+addEvent(g_FINISH_JOB, true)
+addEventHandler(g_FINISH_JOB, resourceRoot, function(id)
+	currentJobProgress = 0
+	honkProgress = 0
+	if isTimer(timer) then
+		killTimer(timer)
+	end
+end)
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	addEventHandler("onClientRender", root, function()
+		if currentJobProgress > 0 then
+			local screenWidth, screenHeight = guiGetScreenSize()
+			local color = tocolor(255, 255, 255)
+
+			dxDrawRectangle(screenWidth / 2 - 126, screenHeight * 0.85, 252, 27, tocolor(0, 0, 0))
+			dxDrawRectangle(screenWidth / 2 - 122, screenHeight * 0.85 + 4, 244, 19, tocolor(40, 40, 40))
+			dxDrawRectangle(screenWidth / 2 - 122, screenHeight * 0.85 + 4, 244 * currentJobProgress, 19, color)
+		end
+
+		if honkProgress > 0 then
+			local screenWidth, screenHeight = guiGetScreenSize()
+			local color = tocolor(255, 255, 255)
+
+			dxDrawRectangle(screenWidth / 2 - 126, screenHeight * 0.85, 252, 27, tocolor(0, 0, 0))
+			dxDrawRectangle(screenWidth / 2 - 122, screenHeight * 0.85 + 4, 244, 19, tocolor(40, 40, 40))
+			dxDrawRectangle(screenWidth / 2 - 122, screenHeight * 0.85 + 4, 244 * honkProgress, 19, color)
+		end
+	end)
+end)
+
+addEvent(g_SELECT_PLAYER_ROLE, true)
+addEventHandler(g_SELECT_PLAYER_ROLE, resourceRoot, function(role)
+	if role == g_CRIMINAL_ROLE then
+		outputChatBox(getPlayerName(localPlayer) .. " is a criminal")
+	else
+		outputChatBox(getPlayerName(localPlayer) .. " is a cop")
+	end
+end)
---
+++ client_perk_ui_logic.lua
@@ -0,0 +1,73 @@
+(function()
+
+local perkAndExplanationUi = nil
+local perkAndExplanationCloseAt = 0
+
+addEvent(g_SHOW_PERK_SELECTION_AND_EXPLANATION, true)
+addEventHandler(g_SHOW_PERK_SELECTION_AND_EXPLANATION, resourceRoot, function()
+	c("g_SHOW_PERK_SELECTION_AND_EXPLANATION")
+
+	showCursor(true)
+	perkAndExplanationUi = guiCreateWindow(0.1, 0.1, 0.8, 0.8, "Instructions and Perks", true)
+	for i, perk in ipairs({
+		g_FUGITIVE_PERK,
+		g_MECHANIC_PERK,
+		g_HOTSHOT_PERK,
+	}) do
+
+		local button = createPerkButton(perkAndExplanationUi, perk, i)
+		addEventHandler("onClientGUIClick", button, function()
+			c("onClientGUIClick " .. perk.name)
+			triggerServerEvent(g_PLAYER_SELECTED_PERK, resourceRoot, perk.id)
+		end, false)
+	end
+
+	-- default select fugitive perk
+	triggerServerEvent(g_PLAYER_SELECTED_PERK, resourceRoot, g_FUGITIVE_PERK.id)
+end)
+
+function createPerkButton(parentElement, perk, position)
+	local width = 0.05
+	local spacing = 0.1
+
+	guiCreateLabel((spacing + width) * position, 0.55, width, 0.05, perk.description, true, parentElement)
+	return guiCreateButton((spacing + width) * position, 0.5, width, 0.05, perk.name, true, parentElement)
+end
+
+addEvent(g_START_PERK_SELECTION_AND_EXPLANATION_TIMER, true)
+addEventHandler(g_START_PERK_SELECTION_AND_EXPLANATION_TIMER, resourceRoot, function(timeToClose)
+	c("g_START_PERK_SELECTION_AND_EXPLANATION_TIMER, time: " .. timeToClose)
+
+	perkAndExplanationCloseAt = nowMs() + timeToClose
+end)
+
+addEvent(g_CLOSE_PERK_SELECTION_AND_EXPLANATION, true)
+addEventHandler(g_CLOSE_PERK_SELECTION_AND_EXPLANATION, resourceRoot, function()
+	c("g_CLOSE_PERK_SELECTION_AND_EXPLANATION")
+
+	showCursor(false)
+	guiSetVisible(perkAndExplanationUi, false)
+end)
+
+addEventHandler("onClientResourceStart", resourceRoot, function()
+	addEventHandler("onClientRender", root, function()
+		local now = nowMs()
+		local screenWidth, screenHeight = guiGetScreenSize()
+
+		if perkAndExplanationCloseAt > now then
+			local timeLeft = perkAndExplanationCloseAt - now
+			dxDrawText(timeLeft / 1000, screenWidth * 0.65 + 2, screenHeight * 0.25 - 2, screenWidth, screenHeight, tocolor(255, 255, 255, 255), 1, "bankgothic")
+			-- incorporate in gui window somehow
+		end
+	end)
+end)
+
+function nowMs()
+	return getRealTime().timestamp * 1000
+end
+
+end)()
+
+function c(...)
+	print("CLIENT", ...)
+end
