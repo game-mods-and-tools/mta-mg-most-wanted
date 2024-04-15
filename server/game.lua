f9030c3565449144b9fc4476046740a31c9424b2
reimu <r2hkri@gmail.com>
2024-04-14 20:27:53 -0500

    add some tick diagnostics

--- server/game.lua
+++ server/game.lua
@@ -42,11 +42,18 @@ function startGameLoop()
 	updateGameState(g_COREGAME_STATE)
 
 	setTimer(function()
+		local start = os.clock()
 		maybeUpdateGameState()
 		maybeSpawnExitPoint()
 		maybeSpawnJob()
 		updateJobProgress()
 		checkPerks()
+		local stop = os.clock()
+		local over = math.floor((stop - start) * 1000 - g_SERVER_TICK_DELAY)
+		if over > 0 then
+			-- things aren't updating as fast as expected
+			outputDebugString("game tick delayed by " .. over .. "ms", 2)
+		end
 	end, g_SERVER_TICK_DELAY, 0)
 end
 
