3d7007eb20417ab18a2ff3392d773fe4cd9a8e57
a <a>
2024-04-21 13:20:51 -0500

    reveal all players near ped

--- server/ped_game.lua
+++ server/ped_game.lua
@@ -1,5 +1,5 @@
-local copFindBlips = {}
-local cops = {}
+local pedFindBlips = {}
+local peds = {}
 local loaded = false
 
 local function startPedRequestListener()
@@ -8,23 +8,27 @@ local function startPedRequestListener()
 		local extortionJobs = getElementsByType("extortion_job", resourceRoot)
 		local randomExtortion = extortionJobs[math.random(#extortionJobs)]
 		local ped = createPed(281, 1272.4, -1337, 13) -- police station
-		cops[#cops + 1] = ped
+		peds[#peds + 1] = ped
 
 		triggerClientEvent(client, g_SPAWN_PLAYER_PED_EVENT, resourceRoot, ped)
 	end)
 
 	setTimer(function()
-		for _, blip in ipairs(copFindBlips) do
+		for _, blip in ipairs(pedFindBlips) do
 			destroyElement(blip)
 		end
-		copFindBlips = {}
-		for _, cop in ipairs(cops) do
+		pedFindBlips = {}
+		for _, cop in ipairs(peds) do
 			local x, y, z = getElementPosition(cop)
 			local detection = getElementsWithinRange(x, y, z, 50, "vehicle")
 			for _, vehicle in ipairs(detection) do
 				local player = getVehicleOccupant(vehicle)
-				if player and getPlayerTeam(player) == g_CriminalTeam then
-					copFindBlips[#copFindBlips + 1] = createBlipAttachedTo(vehicle, 0, 2, 150, 0, 200, 255, 6, 80085)
+				if player then
+					if getPlayerTeam(player) == g_CriminalTeam then
+						pedFindBlips[#pedFindBlips + 1] = createBlipAttachedTo(vehicle, 0, 2, 150, 0, 200, 255, 6, 80085)
+					else
+						pedFindBlips[#pedFindBlips + 1] = createBlipAttachedTo(vehicle, 0, 2, 30, 190, 240, 255, 6, 80085)
+					end
 				end
 			end
 		end
