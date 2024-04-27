682916896bb6af5240d60ebe6c581798590aed4b
a <a>
2024-04-27 08:33:36 -0500

    change spectator respawn text location

--- client/ped_game.lua
+++ client/ped_game.lua
@@ -140,21 +140,21 @@ addEventHandler(g_PED_GAME_READY_EVENT, resourceRoot, function()
 
 
 		if canSpawnAsPedCondition() then
-			dxDrawBorderedText(0.5, "#C8C8C8Press " .. toggleKey .. " to spawn as a pedestrian.", screenWidth / 2, screenHeight - 105,  screenWidth, screenHeight, tocolor(160, 0, 210, 255), 1.9, "arial", center, top, false, false, false, true)
+			dxDrawBorderedText(0.5, "#C8C8C8Press " .. toggleKey .. " to spawn as a pedestrian.", screenWidth / 2, screenHeight - 130,  screenWidth, screenHeight, tocolor(160, 0, 210, 255), 1.9, "arial", center, top, false, false, false, true)
 		elseif playerPed then
 			if focusPed then
-				dxDrawBorderedText(0.5, "Press " .. toggleKey .. " to return to spectator.", screenWidth / 2, screenHeight - 105,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.9, "arial", center, top, false, false, false, true)
+				dxDrawBorderedText(0.5, "Press " .. toggleKey .. " to return to spectator.", screenWidth / 2, screenHeight - 130,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.9, "arial", center, top, false, false, false, true)
 				if not isPedDead(playerPed) then
 					dxDrawBorderedText(0.5, "Your pedestrian will remain vulnerable!", screenWidth / 2, screenHeight - 85,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.2, "arial", center, top, false, false, false, true)
 					dxDrawBorderedText(0.5, "Any player close to you will have their location revealed on the radar to everyone.", screenWidth / 2, screenHeight - 55,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.2, "arial", center, top, false, false, false, true)
 					dxDrawBorderedText(0.5, "Be careful! If you are killed, you will not be able to respawn!", screenWidth / 2, screenHeight - 35,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.2, "arial", center, top, false, false, false, true)
 					dxDrawBorderedText(0.5, "(press 1 to flip the bird, press 2 to wave, press 3 to show your moves)", screenWidth / 2, screenHeight - 15,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.2, "arial", center, top, false, false, false, true)
 				else
-					dxDrawBorderedText(0.5, "Oh dear, you are dead!", screenWidth / 2, screenHeight * 0.5,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 2.8, "sans", center, top, false, false, false, true)
-					dxDrawBorderedText(0.5, "Better luck next time.", screenWidth / 2, screenHeight * 0.5 + 40,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 2.8, "sans", center, top, false, false, false, true)
+					dxDrawBorderedText(0.5, "Oh dear, you are dead!", screenWidth / 2, screenHeight * 0.4,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 2.8, "sans", center, top, false, false, false, true)
+					dxDrawBorderedText(0.5, "Better luck next time.", screenWidth / 2, screenHeight * 0.4 + 40,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 2.8, "sans", center, top, false, false, false, true)
 				end
 			else
-				dxDrawBorderedText(0.5, "Press " .. toggleKey .. " to return to pedestrian.", screenWidth / 2, screenHeight - 105,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.9, "arial", center, top, false, false, false, true)
+				dxDrawBorderedText(0.5, "Press " .. toggleKey .. " to return to pedestrian.", screenWidth / 2, screenHeight - 130,  screenWidth, screenHeight, tocolor(210, 210, 210, 255), 1.9, "arial", center, top, false, false, false, true)
 			end
 		end
 	end)
